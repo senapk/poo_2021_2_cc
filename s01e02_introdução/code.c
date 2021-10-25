015ffffc6c0f8f1430ec88dcf9b49c9d3a5213ce
David Sena <sena.ufc@gmail.com>
2021-10-25 11:59:25 -0300

    up

---
+++ s01e02_introdução/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool existe(int vet[], int size, int value) {
+    for (int i = 0; i < size; i++)
+        if (vet[i] == value)
+            return true;
+    return false;
+}
+
+#define size 5
+
+int main() 
+{
+    int vet[size] = {1, 2, 3, 4, 5};
+    printf("%d\n", existe(vet, size, 3));
+    printf("%d\n", existe(vet, size, 6));
+}
